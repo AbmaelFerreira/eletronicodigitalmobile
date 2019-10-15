2a42701135107c530886a17f61f2fe6646021007
AbmaelFerreira <abmael_ninha@hotmail.com>
2019-10-15 00:38:59 -0400

    Projeto Criado

---
+++ src/main/java/com/eletronicodigitalmobile/EletronicodigitalmobileApplication.java
@@ -0,0 +1,13 @@
+package com.eletronicodigitalmobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletronicodigitalmobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletronicodigitalmobileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eletronicodigitalmobile/EletronicodigitalmobileApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eletronicodigitalmobile;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EletronicodigitalmobileApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
