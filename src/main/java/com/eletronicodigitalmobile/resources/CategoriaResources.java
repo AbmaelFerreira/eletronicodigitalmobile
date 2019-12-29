d86505d8d56df15e554816da1eab2a45f5861fa4
AbmaelFerreira <abmael_ninha@hotmail.com>
2019-12-29 17:48:28 -0400

    34 - Listando todas categorias com DTO

--- src/main/java/com/eletronicodigitalmobile/resources/CategoriaResources.java
+++ src/main/java/com/eletronicodigitalmobile/resources/CategoriaResources.java
@@ -1,8 +1,8 @@
 package com.eletronicodigitalmobile.resources;
 
 import java.net.URI;
-
-import javax.servlet.Servlet;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.eletronicodigitalmobile.domain.Categoria;
+import com.eletronicodigitalmobile.dto.CategoriaDTO;
 import com.eletronicodigitalmobile.service.CategoriaService;
 
 @RestController
@@ -52,6 +53,13 @@ public class CategoriaResources {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+			return ResponseEntity.ok().body(listDto);
 	}
 
 }
--- src/main/java/com/eletronicodigitalmobile/service/CategoriaService.java
+++ src/main/java/com/eletronicodigitalmobile/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.eletronicodigitalmobile.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,30 +18,40 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
+	
+	
 	public Categoria find(Integer id) { 
 		Optional<Categoria> obj = repo.findById(id); 
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-} 
+    } 
+	
+	
 	
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
 	
+	
+	
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
 	
+	
+	
 	public void delete(Integer id) {
 		find(id);
 		try {
-				repo.deleteById(id); //Conforme atualização do material de apoio
-				
+			repo.deleteById(id); //Conforme atualização do material de apoio
 		} catch (DataIntegrityViolationException e) {
 			throw new  DateIntegratyException("Não é possível excluir uma categoria que possue produtos");
 		}
 	}
-
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
 }
