a37c43a4892b83b0e33a7c9d637bc3ec8d3bad4c
AbmaelFerreira <abmael_ninha@hotmail.com>
2020-01-16 06:40:47 -0400

    53 - Criando o profile de teste

---
+++ src/main/java/com/eletronicodigitalmobile/config/TesteConfig.java
@@ -0,0 +1,24 @@
+package com.eletronicodigitalmobile.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.eletronicodigitalmobile.service.DbService;
+
+@Configuration
+@Profile("teste")
+public class TesteConfig {
+	@Autowired
+	private DbService dbservice;
+	
+	@Bean
+	public boolean instanciateDatabase() throws ParseException {
+		dbservice.instantiateTesteDatabase();
+		return true;
+	}
+
+}
