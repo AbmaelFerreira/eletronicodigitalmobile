7f92186c1b8f398d05990454bb875b0a3945c10f
AbmaelFerreira <abmael_ninha@hotmail.com>
2020-02-25 09:47:20 -0400

    70 - Implementando autorização

--- src/main/java/com/eletronicodigitalmobile/config/SecurityConfig.java
+++ src/main/java/com/eletronicodigitalmobile/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import com.eletronicodigitalmobile.security.JWTAuthenticationFilter;
+import com.eletronicodigitalmobile.security.JWTAuthorizationFilter;
 import com.eletronicodigitalmobile.security.JWTUtil;
 
 
@@ -60,6 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter( authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter( authenticationManager(), jwtUtil,userDetailService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
---
+++ src/main/java/com/eletronicodigitalmobile/security/JWTAuthorizationFilter.java
@@ -0,0 +1,53 @@
+package com.eletronicodigitalmobile.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter{
+	
+	private JWTUtil jwtutil;
+	
+	UserDetailsService userdetailservice;
+
+	public JWTAuthorizationFilter(AuthenticationManager authenticationManager, JWTUtil jwtutil, UserDetailsService userdetailservice) {
+		super(authenticationManager);
+		this.jwtutil = jwtutil;
+		this.userdetailservice = userdetailservice;
+	}
+	
+	protected void doFilterInternal(HttpServletRequest request,
+									HttpServletResponse response,
+									FilterChain chain) throws IOException, ServletException{
+		String header = request.getHeader("Authorization");
+		
+		if(header != null && header.startsWith("Bearer " )) {
+			UsernamePasswordAuthenticationToken auth = getAutheticantion(request,header.substring(7));
+			
+			if (auth != null) {
+				SecurityContextHolder.getContext().setAuthentication(auth);
+			}
+		}
+		
+		chain.doFilter(request, response);
+	}
+
+	private UsernamePasswordAuthenticationToken getAutheticantion(String token) {
+		if(jwtutil.tokenValido(token)) {
+			String username = jwtutil.getUsername(token);
+			 UserDetails user = userdetailservice.loadUserByUsername(username);
+			 return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+		}
+		return null;
+	}
+}
