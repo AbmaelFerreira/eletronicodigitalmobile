b40ab574dede2085c77038a1ac0be36306420123
AbmaelFerreira <abmael_ninha@hotmail.com>
2020-09-10 07:14:16 -0400

    78-Esqueci a senha

--- src/main/java/com/eletronicodigitalmobile/service/EmailService.java
+++ src/main/java/com/eletronicodigitalmobile/service/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.eletronicodigitalmobile.domain.Cliente;
 import com.eletronicodigitalmobile.domain.Pedido;
 
 public interface EmailService {
@@ -14,7 +15,9 @@ public interface EmailService {
 	
 	void sendOrderConfirmationHtmlEmail(Pedido obj); 
 	
-	void sendHtmlEmail(MimeMessage msg); 
+	void sendHtmlEmail(MimeMessage msg);
+
+	void sendNewPasswordEmail(Cliente cliente, String newPass); 
 	 
 	
 		
